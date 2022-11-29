cdf45bbcdab70aa0b912cfce1068ca5ffbcc0fc4
Matwy <mattiabasket2004@gmail.com>
2022-11-29 19:05:31 +0100

    registrazione finita

--- Server/controller/user.php
+++ Server/controller/user.php
@@ -2,9 +2,32 @@
 
 require_once'./db_connect.php';
 
+function db_value_already_exists($db_property, $value){
+    // return false if $value is not present in column $db_property
+    // otherwise return row where it exists
+    global $db;
+
+    $sql = "SELECT * from Users where $db_property='". $value ."'";
+    $result = $db->query($sql)->fetch();
+    
+    return $result;
+}
 function register_user($username, $email, $password){
     global $db;
     
+    // check if username and email already exists
+    if(db_value_already_exists('username', $username)){
+        http_response_code(400);
+        echo 'Username already exists';
+        exit();
+    }
+    if(db_value_already_exists('email', $email)){
+        http_response_code(400);
+        echo 'Email already exists';
+        exit();
+    }
+
+    //insert user into database
     $sql = "INSERT INTO Users (username, email, pwd) VALUES ('$username', '$email', '$password')";
     $result = $db->query($sql);
     
@@ -15,6 +38,7 @@ function register_user($username, $email, $password){
         exit();
     }
     else{
+        http_response_code(400);
         echo 'failed to register';
     }
        
@@ -24,9 +48,15 @@ function get_user_info(){
     global $db;
     $id = $_SESSION['userID'];
 
-    if (isset($id)){
-        $sql = "SELECT * from Users where id=$id ";
-        $result = $db->query($sql)->fetch();
-        echo $result->row();
+    if (!isset($id)){
+        http_response_code(403);
+        echo 'user not logged';
     }
+
+    $sql = "SELECT username, email from Users where id=$id ";
+    $result = $db->query($sql)->fetch(PDO::FETCH_ASSOC);
+    
+    http_response_code(200);
+    echo json_encode($result);
+    exit();   
 }
